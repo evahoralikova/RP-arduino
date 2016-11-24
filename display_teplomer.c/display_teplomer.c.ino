76d04994d9f569edd6b897da4f8ffa142e51df47
Eva Horalikova <eva.horalikova@gmail.com>
2016-11-24 20:19:18 +0100

    combined projects 3 and 9

---
+++ display_teplomer.c/display_teplomer.c.ino
@@ -0,0 +1,123 @@
+/*
+  Arduino Starter Kit example
+ Project 11  - Crystal Ball
+
+ This sketch is written to accompany Project 11 in the
+ Arduino Starter Kit
+
+ Parts required:
+ 220 ohm resistor
+ 10 kilohm resistor
+ 10 kilohm potentiometer
+ 16x2 LCD screen
+ tilt switch
+
+
+ Created 13 September 2012
+ by Scott Fitzgerald
+
+ http://www.arduino.cc/starterKit
+
+ This example code is part of the public domain
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// set up a constant for the tilt switchPin
+const int switchPin = 6;
+
+// variable to hold the value of the switchPin
+int switchState = 0;
+
+// variable to hold previous value of the switchpin
+int prevSwitchState = 0;
+
+// a variable to choose which reply from the crystal ball
+int reply;
+
+const int sensorPin = A0;
+
+void setup() {
+  // set up the number of columns and rows on the LCD
+  lcd.begin(16, 2);
+
+  // set up the switch pin as an input
+  pinMode(switchPin, INPUT);
+
+  // Print a message to the LCD.
+  lcd.print("Ask the");
+  // set the cursor to column 0, line 1
+  // line 1 is the second row, since counting begins with 0
+  lcd.setCursor(0, 1);
+  // print to the second line
+  lcd.print("Crystal Ball!");
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // check the status of the switch
+  switchState = digitalRead(switchPin);
+
+  // compare the switchState to its previous state
+  if (switchState != prevSwitchState) {
+    // if the state has changed from HIGH to LOW
+    // you know that the ball has been tilted from
+    // one direction to the other
+    if (switchState == LOW) {
+      // randomly chose a reply
+      reply = random(8);
+      // clean up the screen before printing a new reply
+      lcd.clear();
+      // set the cursor to column 0, line 0
+      lcd.setCursor(0, 0);
+      // print some text
+      lcd.print("the ball says:");
+      // move the cursor to the second line
+      lcd.setCursor(0, 1);
+    }  
+    
+// read the value on AnalogIn pin 0
+  // and store it in a variable
+  int sensorVal = analogRead(sensorPin);
+
+  // send the 10-bit sensor value out the serial port
+  Serial.print("sensor Value: ");
+  Serial.print(sensorVal);
+
+  // convert the ADC reading to voltage
+  float voltage = (sensorVal / 1024.0) * 5.0;
+
+  // Send the voltage level out the Serial port
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  // convert the voltage to temperature in degrees C
+  // the sensor changes 10 mV per degree
+  // the datasheet says there's a 500 mV offset
+  // ((volatge - 500mV) times 100)
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - .5)  * 100;
+  Serial.println(temperature);
+  lcd.clear();
+  // set the cursor to column 0, line 0
+  lcd.setCursor(0, 0);
+  // print some text
+  lcd.print("temperature");
+
+  lcd.setCursor(0, 1);
+  // print some text
+  lcd.print(temperature);
+ 
+
+  }
+  // save the current switch state as the last state
+  prevSwitchState = switchState;
+
+
+}
+
