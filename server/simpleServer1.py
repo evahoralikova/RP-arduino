720e3a36f76ff8728a6392a71a85b2511ebf7c03
Eva Horalikova <eva.horalikova@gmail.com>
2016-12-27 15:27:26 +0100

    handler for get method

--- server/simpleServer1.py
+++ server/simpleServer1.py
@@ -1,11 +1,31 @@
-import SimpleHTTPServer
-import SocketServer
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-PORT = 8000
+PORT_NUMBER = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+	
+	#Handler for the GET requests
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		self.wfile.write("Hello World !")
+		return
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-httpd.serve_forever()
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+	
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+	
