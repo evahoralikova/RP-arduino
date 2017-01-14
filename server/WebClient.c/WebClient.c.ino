049f345a5758a1f930953c3354e3081febdcc695
Eva Horalikova <eva.horalikova@gmail.com>
2017-01-14 20:17:33 +0100

    sends measured temp as an interger

--- server/WebClient.c/WebClient.c.ino
+++ server/WebClient.c/WebClient.c.ino
@@ -1,6 +1,15 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+
+
+
+
+// named constant for the pin the sensor is connected to
+const int sensorPin = A0;
+// room temperature in Celcius
+const float baselineTemp = 20.0;
+
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -19,6 +28,20 @@ IPAddress ip(192, 168, 0, 177);
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
+char *ftoa(char *a, double f, int precision)
+{
+ long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+ 
+ char *ret = a;
+ long heiltal = (long)f;
+ itoa(heiltal, a, 10);
+ while (*a != '\0') a++;
+ *a++ = '.';
+ long desimal = abs((long)((f - heiltal) * p[precision]));
+ itoa(desimal, a, 10);
+ return ret;
+}
+
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
@@ -26,7 +49,30 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.println("1");
+   int sensorVal = analogRead(sensorPin);
 
+  // send the 10-bit sensor value out the serial port
+  Serial.print("sensor Value: ");
+  Serial.print(sensorVal);
+
+  // convert the ADC reading to voltage
+  float voltage = (sensorVal / 1024.0) * 5.0;
+
+  // Send the voltage level out the Serial port
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  // convert the voltage to temperature in degrees C
+  // the sensor changes 10 mV per degree
+  // the datasheet says there's a 500 mV offset
+  // ((volatge - 500mV) times 100)
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - .5)  * 100;
+  Serial.println(temperature);
+  char tempStr[2];
+  itoa(temperature, tempStr,10);
+  Serial.println(tempStr);
+  
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -54,10 +100,12 @@ void setup() {
     client.println("POST /post-temperature.php HTTP/1.1");
     client.println("Host: ec2-35-163-23-102.us-west-2.compute.amazonaws.com");
     client.println("Accept: */*");
-    client.println("Content-Length: 7");
     client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println("7");
     client.println();
-    client.println("temp=23");
+    client.print("temp=");
+    client.println(tempStr);
     /*client.println("GET /view-temperatures.php HTTP/1.1");
     client.println("Host: ec2-35-163-23-102.us-west-2.compute.amazonaws.com");
     client.println("User-Agent: curl/7.50.3");
